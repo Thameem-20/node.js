04a10615abffd1c196edf771832c0c805d9e8d87
Thameem-20 <ahmedthameem20@gmail.com>
2024-07-14 21:54:40 +0530

    updated

---
+++ app.js
@@ -0,0 +1,7 @@
+thameem = {
+    name: 'thameem',
+    usn: '4PA21CS013',
+    sec: 'A'
+}
+
+module.exports=thameem;
--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-console.log("hello world")
+const thameem = require("./app")
+
+console.log("hello world", thameem)
